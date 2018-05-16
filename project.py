7f8c44ff97476e34d322869a5df9db323fb03424
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-05-16 13:54:43 -0300

    Template menu.html

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -12,18 +12,41 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for i in items:
-        output += '<h1>'
-        output += i.name
-        output += '</h1>'
-        # output += '</br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
+    # output = ''
+    # for i in items:
+    #     output += '<h3>'
+    #     output += i.name
+    #     output += '</br>'
+    #     output += i.price
+    #     output += '</br>'
+    #     output += i.description
+    #     output += '</h3>'
+    #     output += '<hr>'
+    #     # output += '</br>'
+    # return output
+
+# Route for newMenuItem
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return 'CRIAR NOVO'
+
+# Route for edit a menu item
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return 'EDITAR'
+
+# Route for delete a menu item
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return 'DELETAR'
+
+
 
 if __name__ == '__main__':
-    # app.debug = True
+    app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
