cbe7002573d3573142f2459ab8ea8f9b39b83b73
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-05-17 14:15:47 -0300

    style.css e JSON

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 # from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import sessionmaker
@@ -14,6 +14,16 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# Making an API Endpoint (GET Request)
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 
 @app.route('/')
 @app.route('/restaurant/<int:restaurant_id>/')
